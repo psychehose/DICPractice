48b6d902bc662c946e1ec0226802ee90fefb8ec3
psychehose <psychehose@gmail.com>
2022-10-12 01:10:16 +0900

    A,B의 공통 상위 모듈인 App Module ( App delegate에 -injectItem 생성

---
+++ Projects/Application/Sources/InjectItems/FeatureAInjectItem.swift
@@ -0,0 +1,11 @@
+//
+//  FeatureAInjectItem.swift
+//  DICPractice
+//
+//  Created by 김호세 on 2022/10/12.
+//  Copyright © 2022 com.devhose. All rights reserved.
+//
+
+import Foundation
+import FeatureDependencies
+import FeatureA
---
+++ Projects/Application/Sources/InjectItems/FeatureBInjectItem.swift
@@ -0,0 +1,11 @@
+//
+//  FeatureBInjectItem.swift
+//  DICPractice
+//
+//  Created by 김호세 on 2022/10/12.
+//  Copyright © 2022 com.devhose. All rights reserved.
+//
+
+import Foundation
+import FeatureDependencies
+import FeatureB
